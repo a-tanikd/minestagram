c0529724057bacdf5c1504ea05387cf11d59f7d3
a-tanikd <8575113+a-tanikd@users.noreply.github.com>
2021-06-02 18:33:25 +0900

    Update App
    - Add route to dashboard
    - Add functionality to store user login state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTE from './constants/route';
+import UserContext from './context/user';
+import useAuthListener from './hooks/use-auth-listener';
 
 const Login = lazy(() => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/sign-up'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
 const NotFound = lazy(() => import('./pages/not-found'));
 
 export default function App() {
+  const { user } = useAuthListener();
+
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Switch>
-          <Route path={ROUTE.LOGIN} component={Login} exact />
-          <Route path={ROUTE.SIGN_UP} component={SignUp} exact />
-          <Route component={NotFound} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path={ROUTE.LOGIN} component={Login} exact />
+            <Route path={ROUTE.SIGN_UP} component={SignUp} exact />
+            <Route path={ROUTE.DASHBOARD} component={Dashboard} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
