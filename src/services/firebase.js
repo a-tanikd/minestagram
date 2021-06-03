bcee1a7b4d61c8edd8a42030673b595e64f73cb0
a-tanikd <8575113+a-tanikd@users.noreply.github.com>
2021-06-03 23:29:43 +0900

    Add services to update following & follower

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,4 @@
-import { firebase } from '../lib/firebase';
+import { FieldValue, firebase } from '../lib/firebase';
 
 export async function doesUsernameExist(username) {
   const result = await firebase
@@ -23,4 +23,51 @@ export async function getUserByUserId(userId) {
   }));
 
   return user;
+}
+
+export async function getSuggestedProfiles(userId, following) {
+  const result = await firebase
+    .firestore()
+    .collection('users')
+    .where('userId', '!=', userId)
+    .limit(10)
+    .get();
+
+  console.log(`following`, following);
+
+  return result.docs
+    .map((user) => ({ ...user.data(), docId: user.id }))
+    .filter((profile) => !following.includes(profile.userId));
+}
+
+export async function updateLoggedInUserFollowing(
+  loggedInUserDocId,
+  profileUserId,
+  isFollowingProfile
+) {
+  return firebase
+    .firestore()
+    .collection('users')
+    .doc(loggedInUserDocId)
+    .update({
+      following: isFollowingProfile
+        ? FieldValue.arrayRemove(profileUserId)
+        : FieldValue.arrayUnion(profileUserId),
+    });
+}
+
+export async function updateFollowedUserFollower(
+  profileDocId,
+  loggedInUserId,
+  isFollowed
+) {
+  return firebase
+    .firestore()
+    .collection('users')
+    .doc(profileDocId)
+    .update({
+      followers: isFollowed
+        ? FieldValue.arrayRemove(loggedInUserId)
+        : FieldValue.arrayUnion(loggedInUserId),
+    });
 }
