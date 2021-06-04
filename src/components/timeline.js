6c734ae5f7e77c2bb4dfc2203c8e6f61701c53d6
a-tanikd <8575113+a-tanikd@users.noreply.github.com>
2021-06-04 09:24:56 +0900

    Implement skeleton of Timeline component

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,21 @@
+/* eslint-disable no-nested-ternary */
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
+
 export default function Timeline() {
+  const { photos } = usePhotos();
+
+  console.log(`photos`, photos);
+
   return (
     <div className="container col-span-2">
-      <p>I am the timeline</p>
+      {!photos ? (
+        <Skeleton count={4} with={640} height={500} className="mb-5" />
+      ) : photos?.length > 0 ? (
+        photos.map((photo) => <p key={photo.docId}>{photo.imageSrc}</p>)
+      ) : (
+        <p className="text-center text-2xl">Follow peopele to see photos</p>
+      )}
     </div>
   );
 }
