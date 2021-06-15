63d5803003e860cadc1aa3179a89409047f675bc
a-tanikd <8575113+a-tanikd@users.noreply.github.com>
2021-06-15 14:17:27 +0900

    Fix usePhotos to receive user instead of use context

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,21 +1,20 @@
 /* eslint-disable no-nested-ternary */
+import { useContext } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import usePhotos from '../hooks/use-photos';
 import Post from './post';
+import LoggedInUserContext from '../context/logged-in-user';
 
 export default function Timeline() {
-  const { photos } = usePhotos();
-
-  console.log(`photos`, photos);
+  const { user } = useContext(LoggedInUserContext);
+  const { photos } = usePhotos(user);
 
   return (
     <div className="container col-span-2">
       {!photos ? (
         <Skeleton count={4} with={640} height={500} className="mb-5" />
-      ) : photos?.length > 0 ? (
-        photos.map((photo) => <Post key={photo.docId} content={photo} />)
       ) : (
-        <p className="text-center text-2xl">Follow peopele to see photos</p>
+        photos.map((photo) => <Post key={photo.docId} content={photo} />)
       )}
     </div>
   );
