93557dcf90c7e1bc67b61026521a89f1d0aa1f02
a-tanikd <8575113+a-tanikd@users.noreply.github.com>
2021-06-15 13:51:03 +0900

    Fix useUser to receive userId instead of use context

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,20 +3,25 @@ import PropTypes from 'prop-types';
 import Header from '../components/header';
 import Sidebar from '../components/sidebar';
 import Timeline from '../components/timeline';
+import useUser from '../hooks/use-user';
+import LoggedInUserContext from '../context/logged-in-user';
 
 export default function Dashboard({ user: loggedInUser }) {
+  const { user } = useUser(loggedInUser.uid);
   useEffect(() => {
     document.title = 'Minestagram';
   }, []);
 
   return (
-    <div className="bg-gray-background">
-      <Header />
-      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
-        <Timeline />
-        <Sidebar />
+    <LoggedInUserContext.Provider value={{ user }}>
+      <div className="bg-gray-background">
+        <Header />
+        <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+          <Timeline />
+          <Sidebar />
+        </div>
       </div>
-    </div>
+    </LoggedInUserContext.Provider>
   );
 }
 
